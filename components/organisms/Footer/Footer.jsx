8653c57225e7aecacc047c9f3e2b7bba636c8757
minechoo <choominhae@gmail.com>
2023-08-31 11:09:10 +0900

    페이지전환효과 Layout으로

--- components/organisms/Footer/Footer.jsx
+++ components/organisms/Footer/Footer.jsx
@@ -4,14 +4,23 @@ import clsx from 'clsx';
 import { Text } from '@/components/atoms/text/Text';
 import { BounceLoader } from 'react-spinners';
 import Btn from '@/components/atoms/Button/Btn';
+import { useGlobalData } from '@/hooks/useGlobalContext';
 
 function Footer() {
+	const { setTheme } = useGlobalData();
 	return (
 		<footer className={clsx(styles.footer)}>
 			<nav>
-				<Btn>Orange</Btn>
-				<Btn>Aqua</Btn>
-				<Btn>HotPink</Btn>
+				{['Orange', 'Aqua', 'Hotpink'].map((el, idx) => (
+					<Btn key={idx} onClick={() => setTheme(`theme${idx + 1}`)}>
+						{el}
+					</Btn>
+				))}
+				{/* 
+				<Btn onClick={() => setTheme('theme1')}>Orange</Btn>
+				<Btn onClick={() => setTheme('theme2')}>Aqua</Btn>
+				<Btn onClick={() => setTheme('theme3')}>HotPink</Btn> 
+				*/}
 			</nav>
 			<Text type={'util'} style={{ letterSpacing: 2 }}>
 				2023 Decodelab All rights reserved.
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useRouter } from 'next/router';
-import { motion, AnimatePresence } from 'framer-motion';
 import { keepStyle } from '@/libs/keepStyle';
 import { GlobalProvider } from '@/hooks/useGlobalContext';
 
@@ -20,29 +19,10 @@ export default function App({ Component, pageProps }) {
 	return (
 		<GlobalProvider>
 			<QueryClientProvider client={queryClient}>
-				<AnimatePresence mode='wait'>
-					<motion.div key={router.pathname}>
-						<Layout>
-							<Component {...pageProps} />
-						</Layout>
-						{/* 페이지가 바뀌기 시작할때 나타날 프레임 */}
-						<motion.div
-							className='in'
-							initial={{ scaleX: 0 }}
-							animate={{ scaleX: 0 }}
-							exit={{ scaleX: 1 }}
-							transition={{ duration: 0.7, ease: [0.25, 0.1, 0.03, 0.99] }}
-						></motion.div>
-						{/* 페이지가 바뀌고 나서 사라질 프레임 */}
-						<motion.div
-							className='out'
-							initial={{ scaleX: 1 }}
-							animate={{ scaleX: 0 }}
-							exit={{ scaleX: 0 }}
-							transition={{ duration: 0.7, ease: [0.25, 0.1, 0.03, 0.99] }}
-						></motion.div>
-					</motion.div>
-				</AnimatePresence>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+
 				<ReactQueryDevtools />
 			</QueryClientProvider>
 		</GlobalProvider>
